4c7bd6cb3ba693042586b7de5843ff02b36bc61a
Renan Goncalves <renan.g200@gmail.com>
2021-08-07 14:40:34 -0300

    First commit

---
+++ android/app/src/main/kotlin/br/com/renangoncalves/tudo_gostoso/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.renangoncalves.tudo_gostoso
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/style.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/painting.dart';
+
+//Meu css
+Color orangeTheme = Color(0xFFFF3D0A);
